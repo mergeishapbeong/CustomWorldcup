00c4e36e1146ddbb525341bedaeccb4029b735cf
Siwon-Kim <siwonkim1108@gmail.com>
2023-05-05 20:29:53 +0900

    style: router 연결을 index.js로 옮김

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const worldcupRouter = require("./worldcup.routes");
+const authRouter = require("./auth.routes");
+const likeRouter = require("./like.routes");
+const mypageRouter = require("./mypage.routes");
+const commentRouter = require("./comment.routes");
+
+router.use("/worldcup", [worldcupRouter, commentRouter, likeRouter]);
+router.use("/auth", [authRouter]);
+router.use("/mypage", [mypageRouter]);
+
 module.exports = router;
