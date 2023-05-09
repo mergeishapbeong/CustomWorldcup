28df0602733be14a7e241feb7d2e3b94bd546dd0
Siwon-Kim <siwonkim1108@gmail.com>
2023-05-09 14:43:04 +0900

    test: jest-html-reporter 반영

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-    "testPathIgnorePatterns": ["/node_modules/"],
-    verbose: true,
-  }
+  testPathIgnorePatterns: ["/node_modules/"],
+  verbose: true,
+  reporters: [
+    "default",
+    [
+      "jest-html-reporters",
+      {
+        publicPath: "./utils/jest-test-report",
+        filename: "report.html",
+        openReport: true,
+        includeFailureMsg: true,
+      },
+    ],
+  ],
+};
